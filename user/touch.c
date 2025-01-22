81158f41e305af49faab3165fe54deb01e8d2fdf
Cheychou Junior <cheychoumouafo@gmail.com>
2025-01-22 12:31:30 +0100

    enhanced touch command

--- user/touch.c
+++ user/touch.c
@@ -6,20 +6,21 @@
 int
 main(int argc, char *argv[])
 {
-    int fd;
+    int fd,i;
 
     if(argc < 2){
         printf("Usage: touch filename\n");
         exit(1);
     }
-
-    // Create file if it doesn't exist, or update timestamp
-    fd = open(argv[1], O_RDWR | O_CREATE);
-    if(fd < 0){
-        printf("touch: cannot create %s\n", argv[1]);
-        exit(1);
-    }
-    close(fd);
+    for(i=1; i<argc; i++){
+        // Create file if it doesn't exist, or update timestamp
+        fd = open(argv[i], O_RDWR | O_CREATE);
+        if(fd < 0){
+            printf("touch: cannot create %s\n", argv[i]);
+            exit(1);
+        }
+        close(fd);
+    }       
 
     exit(0);
 }
